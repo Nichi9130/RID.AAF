575d11e0d7cf168c425a1f01518bbb3c8f3def99
Nichi9130 <djdainichi9173@gmail.com>
2015-12-16 14:10:47 -0500

    Added ApplicationConfigurationModel to Models folder

---
+++ BusinessLayer/Models/ConfigurationModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+
+namespace BusinessLayer.Models
+{
+    internal class ConfigurationModel
+    {
+        private static RetrieveData RetrieveDataHelper = new RetrieveData();
+
+        private DataTable configurationItems =
+            RetrieveDataHelper.RetrieveAppConfigurations("A9ECBCDB-DB27-45C3-80EA-DC4971B72130");
+    }
+}
