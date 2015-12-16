d66ad355e6a90611a50d9d4407f54c5131c40232
Nichi9130 <djdainichi9173@gmail.com>
2015-12-16 16:06:20 -0500

    updating commit syncing project

--- BusinessLayer/CreateApps.cs
+++ BusinessLayer/CreateApps.cs
@@ -63,6 +63,15 @@ namespace BusinessLayer
 
         #region // AddAppConfiguration
 
+        /// <summary>
+        /// AddAppConfiguration adds configurations for an application
+        /// </summary>
+        /// <param name="appConfiguration">variable for the application name</param>
+        /// <remarks>
+        /// <para> AddAppConfiguration adds configurations for selected application
+        /// </para></remarks>
+        /// <remarks> Created by TSH.Dev </remarks>
+      
         public void AddAppConfiguration(string appConfiguration)
         {
             var commandConfig = "[DBO].[InsertApplicationConfigurations]";
@@ -71,7 +80,17 @@ namespace BusinessLayer
             {
                 using (var command = new SqlCommand(commandConfig, conn) {CommandType = CommandType.StoredProcedure})
                 {
-                    
+                    command.Parameters.Add("@ApplicationName", SqlDbType.VarChar).Value = "Test1";
+                    command.Parameters.Add("@AppGuid", SqlDbType.UniqueIdentifier).Value =
+                        "B73014E1-21731-4015-AB65-BF0A1C442130";
+                    command.Parameters.Add("@Description", SqlDbType.VarChar).Value = "Test3";
+                    command.Parameters.Add("@LastModifiedBy", SqlDbType.VarChar).Value = "Test4";
+                    command.Parameters.Add("@LastModifiedDateTime", SqlDbType.DateTime).Value = "";
+                 // command.Parmeters.Add("@ConfigurationTable", SqlDbType.UniqueIdentifier).Value = "Test5" review with DJ
+                    conn.Open();
+                    command.ExecuteNonQuery(); // Built project threw FormatException
+                    conn.Close();
+                    command.Parameters.Clear();
                 }
             }
 
